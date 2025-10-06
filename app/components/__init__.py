59659336a542623b6aec5e9476d2e1d84846a69e
maximosanchezpccomp <maximo.sanchez@pccomponentes.com>
2025-10-06 14:57:34 +0200

    Create __init__.py

---
+++ app/components/__init__.py
@@ -0,0 +1,11 @@
+"""
+Componentes de la aplicación - Módulo de procesamiento y visualización
+"""
+
+from app.components.data_processor import DataProcessor
+from app.components.visualizer import KeywordVisualizer
+
+__all__ = [
+    'DataProcessor',
+    'KeywordVisualizer',
+]
