85d60888bf59a334db52fe3ab1a2a02a67586832
maximosanchezpccomp <maximo.sanchez@pccomponentes.com>
2025-10-06 15:08:26 +0200

    Create __init__.py

---
+++ app/utils/__init__.py
@@ -0,0 +1,47 @@
+"""
+Utilidades y funciones auxiliares
+"""
+
+from app.utils.helpers import (
+    export_to_excel,
+    export_to_json,
+    calculate_metrics,
+    format_number,
+    categorize_keyword_intent,
+    filter_keywords_by_intent,
+    detect_keyword_patterns,
+    create_content_calendar,
+    validate_dataframe
+)
+
+from app.utils.prompts import (
+    build_prompt,
+    THEMATIC_ANALYSIS_PROMPT,
+    INTENT_ANALYSIS_PROMPT,
+    FUNNEL_ANALYSIS_PROMPT,
+    COMPETITIVE_ANALYSIS_PROMPT,
+    GAPS_DETECTION_PROMPT,
+    TRENDS_DETECTION_PROMPT
+)
+
+__all__ = [
+    # helpers
+    'export_to_excel',
+    'export_to_json',
+    'calculate_metrics',
+    'format_number',
+    'categorize_keyword_intent',
+    'filter_keywords_by_intent',
+    'detect_keyword_patterns',
+    'create_content_calendar',
+    'validate_dataframe',
+    
+    # prompts
+    'build_prompt',
+    'THEMATIC_ANALYSIS_PROMPT',
+    'INTENT_ANALYSIS_PROMPT',
+    'FUNNEL_ANALYSIS_PROMPT',
+    'COMPETITIVE_ANALYSIS_PROMPT',
+    'GAPS_DETECTION_PROMPT',
+    'TRENDS_DETECTION_PROMPT',
+]
